9bc82e32a86ac3d95f048eaaf96690b6b19aab51
Alex Tilgreen Mogensen <alext@itu.dk>
2025-06-06 12:02:30 +0700

    brightness slider works! + default image of a cutie :)

--- controller/controller.py
+++ controller/controller.py
@@ -7,29 +7,47 @@ class AppController:
         self.view = view
         self._connect_signals()
 
+        self.load_image("assets/default.png")
+
     def _connect_signals(self):
         sliders = self.view.sliders
-        sliders.pixelAmountChanged.connect(lambda val: self.model.update_slider("pixel_amount", val))
-        sliders.pixelAmountChanged.connect(lambda val: self.model.update_slider("brightness", val))
-        sliders.pixelAmountChanged.connect(lambda val: self.model.update_slider("saturation", val))
-        sliders.pixelAmountChanged.connect(lambda val: self.model.update_slider("contrast", val))
+        sliders.pixelAmountChanged.connect(lambda val: self._slider_changed("pixel_amount", val))
+        sliders.brightnessChanged.connect(lambda val: self._slider_changed("brightness", val))
+        sliders.saturationChanged.connect(lambda val: self._slider_changed("saturation", val))
+        sliders.contrastChanged.connect(lambda val: self._slider_changed("contrast", val))
 
         self.view.options.open_button.clicked.connect(self.open_image)
         self.view.options.save_as_button.clicked.connect(self.save_image)
+
+    def reset_parameters(self):
+        self.model.pixel_amount = 50
+        self.model.brightness = 50
+        self.model.saturation = 50
+        self.model.contrast = 50
+
+        sliders = self.view.sliders
+        sliders.pixelAmountSlider.setValue(50)
+        sliders.brightnessSlider.setValue(50)
+        sliders.saturationSlider.setValue(50)
+        sliders.contrastSlider.setValue(50)
 
     def open_image(self):
         path, _ = QFileDialog.getOpenFileName(self.view, "Open Image", "", "Image Files (*.png *.jpg *.jpeg *.bmp)")
         if not path:
             return
 
+        self.load_image(path)
+
+    def load_image(self, path):
         image = Image.open(path).convert("RGB")
         resized = self.resize_to_fit(image, 400, 400)
+        self.reset_parameters()
         self.view.options.save_as_button.setEnabled(True)
         self.model.load_image(resized)
         self.view.preview.set_image(resized)
 
     def save_image(self):
-        image = self.model.image
+        image = self.model.image_edited
         if image is None:
             return
         
@@ -53,3 +71,44 @@ class AppController:
 
     def _slider_changed(self, name, value):
         self.model.update_slider(name, value)
+        self.render_image()
+
+    def render_image(self):
+        image = self.model.image.copy()
+        if image is None:
+            return
+        
+        pixelated_image = self.edit_pixelation(image)
+        brightened_image = self.edit_brightness(image)
+        saturated_image = self.edit_saturation(image)
+        # contrast_image = self.edit_contrast(image)
+
+        self.model.image_edited = image
+        self.view.preview.set_image(brightened_image)
+
+    def edit_pixelation(self, image):
+        value = self.model.pixel_amount
+        pixel_size = max(1, value // 10)  # Ensure pixel size is at least 1
+        return image
+
+    def edit_brightness(self, image):
+        raw_value = self.model.brightness
+
+        if raw_value < 50:
+            factor = raw_value / 50 
+        else:
+            factor = 1 + 4 * ((raw_value - 50) / 50) **2  # nonlinear brighten
+
+        for y in range(image.height):
+            for x in range(image.width):
+                r, g, b = image.getpixel((x, y))
+                r = max(0, min(255, int(r * factor)))
+                g = max(0, min(255, int(g * factor)))
+                b = max(0, min(255, int(b * factor)))
+                image.putpixel((x, y), (r, g, b))
+
+        return image
+    
+    def edit_saturation(self, image):
+        value = self.model.saturation
+        return image
--- model/model.py
+++ model/model.py
@@ -1,16 +1,18 @@
 class AppModel:
     def __init__(self):
-        self.pixel_amount = 14
-        self.brightness = 48
-        self.saturation = 59
-        self.contrast = 28
+        self.pixel_amount = 50
+        self.brightness = 50
+        self.saturation = 50
+        self.contrast = 50
         self.image = None
+        self.image_edited = None
     
     def update_slider(self, name, value):
         setattr(self, name, value)
     
     def load_image(self, image):
         self.image = image
+        self.image_edit = image
     
     def get_slider_values(self):
         return {
